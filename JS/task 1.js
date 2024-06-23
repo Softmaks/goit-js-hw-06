62c052673d120f5e366c2457876e10e5e56669de
Softmaks <maxviruk@gmail.com>
2024-06-23 19:08:08 +0300

    1
    1

--- JS/task 1.js
+++ JS/task 1.js
@@ -21,63 +21,33 @@
 // - Метод getOrders об'єкта customer використовує this
 // - Метод addOrder об'єкта customer використовує this
 
-// const customer = {
-//   username: "Mango",
-//   balance: 24000,
-//   discount: 0.1,
-//   orders: ["Burger", "Pizza", "Salad"],
-//   // Change code below this line
-//   getBalance() {
-//     return balance;
-//   },
-//   getDiscount() {
-//     return discount;
-//   },
-//   setDiscount(value) {
-//     discount = value;
-//   },
-//   getOrders() {
-//     return orders;
-//   },
-//   addOrder(cost, order) {
-//     balance -= cost - cost * discount;
-//     orders.push(order);
-//   },
-//   // Change code above this line
-// };
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return balance;
+  },
+  getDiscount() {
+    return discount;
+  },
+  setDiscount(value) {
+    discount = value;
+  },
+  getOrders() {
+    return orders;
+  },
+  addOrder(cost, order) {
+    balance -= cost - cost * discount;
+    orders.push(order);
+  },
+  // Change code above this line
+};
 
-// customer.setDiscount(0.15);
-// console.log(customer.getDiscount()); // 0.15
-// customer.addOrder(5000, "Steak");
-// console.log(customer.getBalance()); // 19750
-// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
-
-class Car {
-  #price;
-
-  static maxPrice = 50000;
-
-  constructor(params) {
-    this.#price = params.price;
-  }
-
-  get price() {
-    return this.#price;
-  }
-  set price(newPrice) {
-    if (this.#price(newPrice) <= car.maxPrice) {
-      this.#price;
-    } else {
-      this.#price = newPrice;
-    }
-  }
-}
-
-const audi = new Car({ price: 35000 });
-console.log(audi.price); // 35000
-
-audi.price = 49000;
-console.log(audi.price); // 49000
-
-audi.price = 51000;
-console.log(audi.price); // 49000
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
